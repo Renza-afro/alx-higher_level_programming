f7e58c7e5776bb783233acbf76b6c46e3735412a
Renza-afro <ratsoane20@gmail.com>
2022-09-20 13:03:54 -0700

    5.00..99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for x in range(99):
-    print('{:02d},'.format(x), ', ' if x < 99 else '\n', sep='', end="")
+    print('{:02d}'.format(x), ', ' if x < 99 else '\n', sep='', end='')
