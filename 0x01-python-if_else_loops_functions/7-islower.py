e1219991e9070c81fff2871fb5a50210db299442
Renza-afro <ratsoane20@gmail.com>
2022-09-20 14:00:28 -0700

    fix

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islowe(c):
-    if ord(c) < ord('z') + 1 and ord(c) > ord(;'a') - 1:
+    if ord(c) < ord('z') + 1 and ord(c) > ord('a') - 1:
         return True
     else:
         return False
