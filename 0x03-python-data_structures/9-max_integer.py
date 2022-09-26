34ccdc03bec78018b4e7e7e5b49d25fcb36c823a
Renza-afro <ratsoane20@gmail.com>
2022-09-26 04:34:42 -0700

    fix indent

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    def max_integer(my_list=[]):
-        if len(my_list) == 0:
-            return None
+    if len(my_list) == 0:
+        return 'None'
+    else:
+
         big = my_list[0]
         for small in my_list:
             if small > big:
                 big = small
-                return big
+        return big
