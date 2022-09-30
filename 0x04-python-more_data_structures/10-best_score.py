175aa226daa12819cb9d57cc8b414cd405d6e726
Renza-afro <ratsoane20@gmail.com>
2022-09-29 19:55:12 -0700

    fix pycodestyle error

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dict):
-    if a_dict is None  or a_dict == {}:
+    if a_dict is None or a_dict == {}:
         return None
     maxA = max(a_dict.values())
     for key in a_dict:
